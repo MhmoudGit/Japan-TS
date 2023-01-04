645309840c6b1b5a4011b1ee6beafba663a68aa4
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-01-04 19:46:17 +0200

    nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 const Header = () => {
-    return ( 
-        <nav>
-            
-        </nav>
-     );
+	return (
+		<nav className='px-10 py-14 flex justify-between items-center font-bold'>
+			<ul className='flex items-center gap-4'>
+				<span>Japan</span>
+				<li>Something</li>
+				<li>Something</li>
+				<li>Something</li>
+				<li>Something</li>
+			</ul>
+			<ul className='flex items-center'>
+				<li></li>
+				<button>Click Here</button>
+			</ul>
+		</nav>
+	)
 }
- 
-export default Header;
+
+export default Header
